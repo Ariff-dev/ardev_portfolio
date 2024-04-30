3bb1a8fd0ac57820bc425434582c68b9b41af009
Ariff-dev <ariff.dev24@gmail.com>
2024-04-30 17:49:57 -0600

    SetUp Project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,23 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        backgroundColor: '#080808',
+        box_backround_primary: '#0F0F0F',
+        box_backround_secundary: '#000000',
+        color_title: '#38B6FF',
+        color_subtext: '#83858C',
+        icon_color: '#38B6FF',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
